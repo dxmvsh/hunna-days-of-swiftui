ff797186e1dc874caacacf8c27daeedd1f1d78d5
dxmvsh <dimash.live@gmail.com>
2022-05-26 23:40:25 +0600

    feat(day1to14): add checkpoint 3 for days 5 & 6

--- day1to14.playground/Contents.swift
+++ day1to14.playground/Contents.swift
@@ -17,3 +17,16 @@ let uniqueWords = Set(words)
 print("Array of words: \(words)")
 print("Number of words: \(words.count)")
 print("Number of unique words: \(uniqueWords.count)")
+
+// Checkpoint 3. Day 5 & 6.
+for number in 1...100 {
+    if number.isMultiple(of: 3) && number.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if number.isMultiple(of: 3) {
+        print("Fizz")
+    } else if number.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(number)
+    }
+}
