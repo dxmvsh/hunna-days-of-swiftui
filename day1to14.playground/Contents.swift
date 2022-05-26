46840e5f7140ac1d67882c69d109664e021673fb
dxmvsh <dimash.live@gmail.com>
2022-05-27 00:10:23 +0600

    feat(day1to14): add checkpoint 5 for days 9

--- day1to14.playground/Contents.swift
+++ day1to14.playground/Contents.swift
@@ -65,3 +65,12 @@ do {
         print("Other error occurred")
     }
 }
+
+// Checkpoint 5. Day 9
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+luckyNumbers.filter({ !$0.isMultiple(of: 2) })
+    .sorted(by: { $0 < $1 })
+    .map({ "\($0) is lucky number" })
+    .forEach({ print($0) })
