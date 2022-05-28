7cd9d376232fb6c1b33f3cee9b5bcb2751b35abe
dxmvsh <dimash.live@gmail.com>
2022-05-29 03:50:39 +0600

    feat(WeSplit): add WeSplit app after finishing days 16, 17 & 18

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Dimash on 29.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tip = checkAmount * tipSelection / 100
+        return tip + checkAmount
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        return grandTotal / peopleCount
+    }
+    
+    var currencyFormat: FloatingPointFormatStyle<Double>.Currency {
+        .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormat)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip you want to leave?")
+                }
+                
+                Section {
+                    Text(grandTotal, format: currencyFormat)
+                } header: {
+                    Text("Grand Total Value")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currencyFormat)
+                } header: {
+                    Text("Amount per person")
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
