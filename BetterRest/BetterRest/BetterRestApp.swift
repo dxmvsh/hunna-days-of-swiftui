c099277c02f3cd545eabc393cccb14df8ee772c1
dxmvsh <dimash.live@gmail.com>
2022-06-06 18:06:53 +0600

    feat(BetterRest): add new project and stepper

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dimash on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
