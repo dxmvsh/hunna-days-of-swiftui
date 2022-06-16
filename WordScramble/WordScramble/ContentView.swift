7efb1bfd32c62bc02bd9a51cbf0638a7fdb6a5f6
dxmvsh <dimash.live@gmail.com>
2022-06-17 00:55:37 +0600

    feat(WordScramble): add implementation of the game

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -10,26 +10,111 @@ import SwiftUI
 struct ContentView: View {
     let people = ["Finn", "Leia", "Luke", "Rey"]
     
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        List(people, id: \.self) {
-            Text($0)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .onSubmit(addNewWord)
+                        .onAppear(perform: startGame)
+                        .alert(errorTitle, isPresented: $showingError) {
+                            Button("OK", role: .cancel) { }
+                        } message: {
+                            Text(errorMessage)
+                        }
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
         }
     }
     
-    private func checker() {
-        let word = "fahter"
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    private func addNewWord() {
+        let word = newWord.lowercased()
+        guard word.count > 0 else { return }
+        guard isOriginal(word: word) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        guard isPossible(word: word) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        guard isReal(word: word) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        withAnimation {
+            usedWords.insert(word, at: .zero)
+        }
+        newWord = ""
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let index = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: index)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
+        let range = NSRange(location: .zero, length: word.count)
         let misspelledRange = checker.rangeOfMisspelledWord(
             in: word,
             range: range,
-            startingAt: 0,
+            startingAt: .zero,
             wrap: false,
             language: "en"
         )
-        let allGood = misspelledRange.location == NSNotFound
-        print(allGood)
+        return misspelledRange.location == NSNotFound
     }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
