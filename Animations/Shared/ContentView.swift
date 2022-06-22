eb5aa2f3173a998d316a9431c20fe3d7329b0503
dxmvsh <dimash.live@gmail.com>
2022-06-22 23:57:32 +0600

    feat(Animations): add scale and blur effect

--- Animations/Shared/ContentView.swift
+++ Animations/Shared/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .animation(.default, value: animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
     }
 }
 
