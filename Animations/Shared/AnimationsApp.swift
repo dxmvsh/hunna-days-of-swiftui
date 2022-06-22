7850a5622354b5af38b96b8f9b278e5d4e98acb0
dxmvsh <dimash.live@gmail.com>
2022-06-22 23:54:15 +0600

    feat(Animations): add Animations project

---
+++ Animations/Shared/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Shared
+//
+//  Created by Dimash on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
